fd7fdbe8690dce4276c5312c7698fcb52d925465
Stelios Ioannides <s_ioannides@outlook.com>
2026-06-09 07:52:23 +0300

    Revert "feat: add FIFA World Cup 2026 web app (core infrastructure, stores, API, data, utils, i18n)"

--- world-cup-2026/src/stores/theme.js
+++
@@ -1,8 +0,0 @@
-import { defineStore } from 'pinia'
-export const useThemeStore = defineStore('theme', {
-  state: () => ({ isDark: localStorage.getItem('wc2026_theme') !== 'light' }),
-  actions: {
-    toggle() { this.isDark = !this.isDark; localStorage.setItem('wc2026_theme', this.isDark ? 'dark' : 'light'); this.apply() },
-    apply() { if (this.isDark) { document.documentElement.classList.remove('light') } else { document.documentElement.classList.add('light') } }
-  }
-})
